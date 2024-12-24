dfc9039d3227d3c113015b14680e4031174b0be9
hilqia_kenda <hilqiakenda0@gmail.com>
2024-12-24 19:38:53 +0300

    building Authentication System

--- store/models.py
+++ store/models.py
@@ -1,7 +1,10 @@
 from django.core.validators import MinValueValidator
+from django.conf import settings
+from django.contrib import admin
 from django.utils.timezone import now
 from django.db import models
 from uuid import uuid4
+
 
 # Create your models here.
 class Promotion(models.Model):
@@ -46,18 +49,25 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLDEN, 'Golden'),
     ]
     
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+    @admin.display(ordering='first_name')
+    def first_name(self):
+        return self.user.first_name
+    
+    @admin.display(ordering='first_name')
+    def last_name(self):
+        return self.user.last_name
     
     class Meta:
-        ordering = ["first_name", "last_name"]
+        ordering = ["user__first_name", "user__last_name"]
 
 class Order(models.Model):
     payment_pending = 'p'
@@ -72,6 +82,11 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=payment_pending)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+    class Meta:
+        permissions = [
+            ('cancel_order', 'can cancel order')
+        ]
     
 class Cart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
